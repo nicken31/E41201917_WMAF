9453c1541ccce447a285e9b535e0bae104101913
nicken-mrgrt <75102286+nicken31@users.noreply.github.com>
2022-03-13 19:32:26 +0700

    delete dart

--- minggu 3/async1.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:async';
-
-void main (){
-print("saya dijalankan pertama");
-var timer = Timer(Duration(seconds: 3), ()=>print('saya dijalankan terakhir'));
-print("saya dijalankan kedua");
-}
