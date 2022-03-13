ca358a73048b728442154cf9e23111e33db9eea7
nicken-mrgrt <75102286+nicken31@users.noreply.github.com>
2022-03-13 19:34:37 +0700

    Add files via upload

---
+++ minggu 3/constructor.dart
@@ -0,0 +1,14 @@
+void main() {
+  Siswa s = new Siswa();
+  s.view();
+}
+
+class Siswa {
+  // field
+  String nama = "Nicken Betrice Margareta";
+
+  // function
+  void view() {
+    print(nama);
+  }
+}
